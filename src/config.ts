b3cd8a9404f47251b93e1091869019cf5c9f7faa
HA-tukur <tukurhammeda@gmail.com>
2026-01-26 01:23:20 +0000

    Updated config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   supabase: {
-    url: import.meta.env.VITE_SUPABASE_URL || 'https://uiupyujiicsqskrrjmjp.supabase.co',
-    anonKey: import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpdXB5dWppaWNzcXNrcnJqbWpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4MjgxOTMsImV4cCI6MjA4MzQwNDE5M30.bWa0DlYLJVmKSUZ2JEadujmco-5PaQCLM-iKBpGYoJ0'
+    url: import.meta.env.VITE_SUPABASE_URL || 'https://bpkelzrvivtxjcqvglsl.supabase.co',
+    anonKey: import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwa2VsenJ2aXZ0eGpjcXZnbHNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3NzU1NDgsImV4cCI6MjA4NDM1MTU0OH0.0DGpvOMaq-IZkHj-6N2_JANeqnF-uLgrO3f_rwfduDM'
   }
 };
