93f2906711ae6b47d7732726f3f4f02d727a32d8
HA-tukur <tukurhammeda@gmail.com>
2026-01-26 00:24:23 +0000

    Updated Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 
 const App = lazy(() => import('./App'));
 const InstructorSetup = lazy(() => import('./components/InstructorSetup').then(m => ({ default: m.InstructorSetup })));
+const InstructorDashboard = lazy(() => import('./components/InstructorDashboard').then(m => ({ default: m.InstructorDashboard })));
 const StudentProfile = lazy(() => import('./components/StudentProfile'));
 const Leaderboard = lazy(() => import('./components/Leaderboard'));
 
@@ -24,7 +25,7 @@ export default function Router() {
           <Route path="/leaderboard" element={<Leaderboard />} />
           <Route path="/profile" element={<StudentProfile />} />
           <Route path="/instructor/setup" element={<InstructorSetup />} />
-          <Route path="/instructor/:classCode" element={<InstructorSetup />} />
+          <Route path="/instructor/:code" element={<InstructorDashboard />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
